5179ed61b637eca8ce5cf6c7c6a88ede7038acf0
chendaiqiang <694150121@qq.com>
2018-12-20 00:03:30 +0800

    20181220

--- 02 Trunk/03 PC源码/src/pages/Redirect.vue
+++ 02 Trunk/03 PC源码/src/pages/Redirect.vue
@@ -19,47 +19,75 @@
                 code: code,
                 noCheck: 1
             };
+            let json = {
+                "msg": "success",
+                "userInfo": {
+                    "createTime": {
+                        "date": 17,
+                        "day": 1,
+                        "hours": 23,
+                        "minutes": 31,
+                        "month": 11,
+                        "seconds": 45,
+                        "time": 1545060705000,
+                        "timezoneOffset": -480,
+                        "year": 118
+                    },
+                    "gender": 1,
+                    "lastTime": {
+                        "date": 17,
+                        "day": 1,
+                        "hours": 23,
+                        "minutes": 31,
+                        "month": 11,
+                        "seconds": 45,
+                        "time": 1545060705000,
+                        "timezoneOffset": -480,
+                        "year": 118
+                    },
+                    "lastValidTime": 1545233361873,
+                    "nickName": "薛利飞",
+                    "openId": "oVEQd0SO9VTr0WkHR4uJCmq7MWdo",
+                    "phone": "13347176976",
+                    "reason": "厉害了",
+                    "roles": [{"remark": "", "roleKey": "writer", "roleName": "录入人员", "roleType": 0}],
+                    // rolekey;admin/管理员,checker/审核人员，writer/录入人员，common/一般员工，visitor/游客
+                    "status": 1, //-1、0、1、2==未注册、待审核、已审核、被拒绝
+                    "uid": 26,
+                    "unionId": "ogOHU1HNbJPVM6izCoA6ZrWaRwms"
+                },
+                "code": 1,
+                "status": 1
+            };
             try {
                 this.$ajax.autoService
                     .login(params)
                     .then((response) => {
                         if (response.status === 200) {
                             let data = response.data;
+                            console.log(data);
                             if (data.msg === "success") {
-                                this.$router.push("/main");
+                                console.log(data);
+                                // this.$router.push("/main");
                             } else {
                                 this.$message.error(data.msg);
-                                setTimeout(()=>{
+                                setTimeout(() => {
                                     this.$router.push("/login");
-                                },2000);
+                                }, 2000);
                             }
                         }
                     }, (error) => {
                         this.$message.error(error);
-                        setTimeout(()=>{
+                        setTimeout(() => {
                             this.$router.push("/login");
-                        },2000);
+                        }, 2000);
                     });
-            } catch(e){
+            } catch (e) {
                 this.$message.error(e);
-                setTimeout(()=>{
+                setTimeout(() => {
                     this.$router.push("/login");
-                },2000);
+                }, 2000);
             }
-            // this.$axios.get(`/login/pcLogin.do?code=${code}&noCheck=1`)
-            //     .then((response) => {
-            //         if (response.status === 200) {
-            //             let data = response.data;
-            //             if (data.msg === "success") {
-            //                 this.$router.push("/main");
-            //             } else {
-            //                 this.$router.push("/login");
-            //             }
-            //         }
-            //     })
-            //     .catch((error) => {
-            //         this.$message(error);
-            //     });
         }
     };
 </script>
