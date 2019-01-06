c5f1b6ac8f37ed6b397576a0a29427076da55a7f
Kandy-He <39376467+Kandy-He@users.noreply.github.com>
2019-01-06 21:41:02 +0800

    修改日志查看
    增加查看多页日志

--- 02 Trunk/04小程序源码/src/pages/my/logs/index.vue
+++ 02 Trunk/04小程序源码/src/pages/my/logs/index.vue
@@ -1,10 +1,13 @@
-<template>
+﻿<template>
   <div>
     <ul class="container log-list">
       <li v-for="(log, index) in logs" :key="index" class="log-item">
         {{log.remark}}
       </li>
     </ul>
+    <div class="more-logs">
+      <span class="get-more-logs" @click="getLogList()">查看更多日志</span>
+    </div>
   </div>
 </template>
 
@@ -12,10 +15,11 @@
 export default {
   data () {
     return {
-      logs: []
+      logs: [],
+      pageSize: 30,
+      pageNumber: 1
     }
   },
-
   mounted () {
     this.getLogList();
   },
@@ -28,14 +32,24 @@ export default {
         url: '/wx/log/query.do',
         method: 'get',
         data: {
-          pageSize: 30,
-          pageNumber: 1
+          pageSize: this.pageSize,
+          pageNumber: this.pageNumber
         },
         success: res => {
-          console.log(res);
-          this.logs = res.data;
-        },
-        fail:() => {
+          //console.log(res);
+          if(res.data.length > 0){
+            this.pageNumber += 1;
+            for(var i=0; i<res.data.length; i++){
+              this.logs.push(res.data[i]);
+            }
+            //this.onLoad();
+          }
+          else {
+            wx.showToast({
+              title: "后面没有日志了！",
+              mask: true
+            });
+          }
         }
       });
     }
@@ -52,7 +66,6 @@ page {
   flex-direction: column;
   padding: 40rpx;
 }
-
 .log-item {
   width: 335px;
   margin-top: 10px;
@@ -60,5 +73,18 @@ page {
   overflow: hidden;
   white-space: nowrap;
   text-overflow: ellipsis;
+}
+.more-logs {
+  height: 32px;
+  width: 100%;
+  font-size: 18px;
+  text-align: center;
+  line-height: 32px;
+}
+.get-more-logs {
+  padding: 4px 7px;
+  color: white;
+  background-color: #E64340;
+  border-radius: 4px;
 }
 </style>
