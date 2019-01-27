6787f3eee6323a6ad394086f519e747a6757a9f4
vienna1981 <41502909+vienna1981@users.noreply.github.com>
2019-01-27 23:09:47 +0800

    更新不依赖/ttzy

--- 02 Trunk/01 服务器/src/main/java/com/grosup/ttzy/resource/service/Imp/ResourceAttrServiceImp.java
+++ 02 Trunk/01 服务器/src/main/java/com/grosup/ttzy/resource/service/Imp/ResourceAttrServiceImp.java
@@ -9,14 +9,18 @@ import org.springframework.stereotype.Service;
 import com.grosup.ttzy.resource.dao.ResourceAttrDao;
 import com.grosup.ttzy.resource.dto.ResourceAttrDto;
 import com.grosup.ttzy.resource.service.ResourceAttrService;
+import com.grosup.ttzy.resource.service.ResourceDetailService;
 
 @Service
-public class ResourceAttrServiceImp  implements ResourceAttrService {
+public class ResourceAttrServiceImp implements ResourceAttrService {
 
 	private static Logger log = Logger.getLogger(ResourceAttrServiceImp.class);
 
 	@Autowired
 	private ResourceAttrDao resourceAttrDao;
+
+	@Autowired
+	ResourceDetailService resourceDetailService;
 
 	public void create(String json) {
 		resourceAttrDao.create(json);
@@ -27,27 +31,32 @@ public class ResourceAttrServiceImp  implements ResourceAttrService {
 		if (resourceAttrDto != null) {
 			resourceAttrDao.add(resourceAttrDto);
 		} else {
-			log.error("resourceAttrService add resourceAttrDto is null. resourceKey json:" + json);
+			log.error("resourceAttrService add resourceAttrDto is null. attrKey json:" + json);
 		}
 
 	}
 
 	public void del(String attrKey) {
 		resourceAttrDao.del(attrKey);
-
 	}
 
 	public void update(String json) {
 		ResourceAttrDto resourceAttrDto = resourceAttrDao.create(json);
 		if (resourceAttrDto != null) {
-			resourceAttrDao.update(resourceAttrDto);
+			resourceAttrDto = resourceAttrDao.update(resourceAttrDto);
+			if (null != resourceAttrDto) {
+				resourceDetailService.updateAttrName(resourceAttrDto.getAttrKey(), resourceAttrDto.getAttrName());
+			}else
+			{
+				log.error("resourceAttrService update resourceAttrDto with attrKey is error. json:" + json);
+			}
 		} else {
-			log.error("resourceAttrService update resourceAttrDto is null. resourceKey json:" + json);
+			log.error("resourceAttrService update resourceAttrDto is null. attrKey json:" + json);
 		}
 	}
 
-	public ResourceAttrDto get(String resourceKey) {
-		return resourceAttrDao.get(resourceKey);
+	public ResourceAttrDto get(String attrKey) {
+		return resourceAttrDao.get(attrKey);
 	}
 
 	public Collection<ResourceAttrDto> getAll(String typeKey) {
